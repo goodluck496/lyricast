a0320efd1c0a161ce848c4dbe416b02eb71ae245
Nikita Kalinin <nikita.kalinin@skillbox.ru>
2025-01-16 21:17:56 +0600

    - mock electron context for run app in browser

--- libs/common-browser/src/lib/services/window.service.ts
+++ libs/common-browser/src/lib/services/window.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Context } from '@lyri-cast/common-electron';
+import { AppWindowTypes, Context } from '@lyri-cast/common-electron';
 
 @Injectable({ providedIn: 'platform' })
 export class WindowService {
+  get hasElectron(): boolean {
+    return ('electron' in (window as Window));
+  }
+
   get electronContext(): Context {
-    return (window as any).electron as Context;
+    return this.hasElectron
+      ? ((window as any).electron as Context)
+      : {
+          send: () => void 0,
+          receive: () => void 0,
+          getAppVersion: () => Promise.resolve(''),
+          getWindowType: () => Promise.resolve(AppWindowTypes.MAIN),
+          closeWindow: () => Promise.resolve(void 0),
+          openWindow: () => Promise.resolve(0),
+          platform: '',
+        };
   }
 }
